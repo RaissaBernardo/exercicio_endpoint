80952a17b3dab6649a4870ad04dfff7fc1320210
RaissaBernardo <raissabernardo.tds24@gmail.com>
2025-02-24 14:03:31 -0300

    feito

--- src/main/java/com/raissa/demo/view/VendaView.java
+++ src/main/java/com/raissa/demo/view/VendaView.java
@@ -1,15 +1,23 @@
 package com.raissa.demo.view;
 
+import com.raissa.demo.banco.DataBaseCliente;
+import com.raissa.demo.banco.DataBaseProduto;
 import com.raissa.demo.controller.VendaController;
+import com.raissa.demo.model.Produto;
 import com.raissa.demo.model.Venda;
+import com.raissa.demo.model.Cliente;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/venda")
 public class VendaView {
     VendaController vd = new VendaController();
+    DataBaseCliente dbClientes = new DataBaseCliente();
+    DataBaseProduto dbProdutos = new DataBaseProduto();
 
     @GetMapping
     public List<Venda> getAllVendas() {
@@ -17,8 +25,28 @@ public class VendaView {
     }
 
     @PostMapping
-    public void addVendas(@RequestBody Venda v) {
+    public String addVendas(@RequestParam int idCliente, @RequestParam List<Integer> idsProdutos) {
+        Cliente cliente = dbClientes.findOne(idCliente);
+        List<Produto> produtos = new ArrayList<>();
+
+        for (int idProd : idsProdutos) {
+            Produto p = dbProdutos.findOne(idProd);
+            if (p != null) {
+                produtos.add(p);
+            }
+        }
+
+        if (cliente == null) {
+            return "Cliente não encontrado!";
+        }
+        if (produtos.isEmpty()) {
+            return "Nenhum produto encontrado!";
+        }
+
+        Venda v = new Venda(null, LocalDate.now(), produtos, cliente);
         vd.adicionarVendas(v);
+
+        return "Venda cadastrada com sucesso!";
     }
 
     @PutMapping
